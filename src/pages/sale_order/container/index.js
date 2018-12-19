fe2d5246ce5ffbae1190c6f00682ba1a2ef37ab5
longxuecheng <longxuecheng@meican.com>
2018-12-19 15:31:54 +0800

    test table edit

--- src/pages/sale_order/container/index.js
+++ src/pages/sale_order/container/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import SaleOrderList from "../pages/sale_list";
 import SaleOrderInfo from "../pages/sale_info"
+import EditableTable from "../components/saleOrderInfoForm/test"
 
 class SaleOrderManageLayouts extends Component {
     render() {
@@ -9,7 +10,7 @@ class SaleOrderManageLayouts extends Component {
             <Fragment>
                 <Route exact path='/salemanage/list' component={SaleOrderList} name='销售订单列表'></Route>
                 <Route exact path='/salemanage/info/:id' component={SaleOrderInfo} name='销售订单详情'></Route>
-                {/* <Route exact path='/goodsmanage/addgoods' component={GoodsInfo} name='新增商品'></Route> */}
+                <Route exact path='/salemanage/test' component={EditableTable} name='测试编辑Table'></Route>
             </Fragment>
         )
     }
